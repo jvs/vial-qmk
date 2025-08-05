c6834e2265db33e054b58226ce84715c662574d6
John Von Seggern <vonseg@protonmail.com>
2025-08-05 08:11:46 -0500

    Temporarily set right thumb key to "z".

--- keyboards/lily58/keymaps/jvs/keymap.c
+++ keyboards/lily58/keymaps/jvs/keymap.c
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    CKC_D,   CKC_F,   KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LALT,  KC_LSFT,KC_Z,    KC_X,    KC_C,    KC_V, KC_B,        KC_B,  KC_N,    CKC_M,   CKC_COMM,KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LGUI, MO(_LOWER), KC_LSFT, QK_LEAD, KC_ENT, KC_SPC, MO(_RAISE), MO(_NUMBER)
+                        KC_LGUI, MO(_LOWER), KC_LSFT, QK_LEAD, KC_ENT, KC_SPC, MO(_RAISE), KC_Z
 ),
 
 /* LOWER
