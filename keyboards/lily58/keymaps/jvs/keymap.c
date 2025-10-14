b064fcb116435c7674e77df0897a1ab8d30ef9bd
John Von Seggern <vonseg@protonmail.com>
2025-10-13 23:05:41 -0500

    Try moving escape chord to the left hand.

--- keyboards/lily58/keymaps/jvs/keymap.c
+++ keyboards/lily58/keymaps/jvs/keymap.c
@@ -97,21 +97,21 @@ static void toggle_os(void) {
 
 // Combos
 enum combo_events {
-    JK_ESC,
+    ER_ESC,
     ALT_ESC_WIN,
     UI_BSPC,      // Right hand: U+I for backspace
     OP_DEL,       // Right hand: O+P for delete
     MCOMM_ENT,    // Right hand: M+comma for enter
 };
 
-const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM alt_esc_combo[] = {KC_LALT, KC_ESC, COMBO_END};
 const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM op_combo[] = {KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM mcomm_combo[] = {HR_M, HR_COMM, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-    [JK_ESC] = COMBO(jk_combo, KC_ESC),
+    [ER_ESC] = COMBO(er_combo, KC_ESC),
     [ALT_ESC_WIN] = COMBO(alt_esc_combo, WIN_SWITCH),
     [UI_BSPC] = COMBO(ui_combo, KC_BSPC),
     [OP_DEL] = COMBO(op_combo, KC_DEL),
